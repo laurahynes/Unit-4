c511ef131b5d80c94291e059053864d87cbb20f2
laur1290 <laur1290@ISS2TVRKH2.scdsb.pri>
2019-11-21 10:53:44 -0500

    Initial commit

---
+++ src/Lesson10Sort/ISSStudent.java
@@ -0,0 +1,32 @@
+package Lesson10Sort;
+
+public class ISSStudent implements Comparable {
+    private String name, address;
+    private int id;
+    
+    public ISSStudent(String n, String a, int i){
+        name = n;
+        address = a;
+        id = i;
+    }
+    
+    public String toString(){
+        String str = "Name: " + name;
+        str+= "\nAddress: " + address;
+        str += "\nID: " + id + "\n------------";
+        return str;
+    }
+    public int getID(){
+        return id;
+    }
+    
+    public String getName(){
+        return name;
+    }
+
+    @Override
+    public int compareTo(Object t) {
+        String otherName = ((ISSStudent)t).getName();
+        return name.compareTo(otherName);
+    }
+}
